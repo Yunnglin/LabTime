150ffae4853eabbec2384a1b6f4e97226818db80
Yunlin <mao.looper@qq.com>
2019-10-25 15:34:17 +0800

    add
    add file

---
+++ Assets/Class/Entity/PowerSource.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Class.Frame;
+
+namespace Assets.Class.Entity
+{
+    /// <summary>
+    /// 电源基实体类：用直接用作普通电源，也可以派生其他复杂电源
+    /// </summary>
+    public class PowerSource:EletronicUnit
+    {
+        protected double EMF = 0;//电动势
+        
+        public PowerSource(double emf,double r)
+        {
+            EMF = emf;
+            R = r;
+            V = -emf;
+        }
+
+        /// <summary>
+        /// 获取电源电动势
+        /// </summary>
+        public double GetEMF()
+        {
+            return EMF;
+        }
+
+        /// <summary>
+        /// 获取正极，默认为右节点
+        /// </summary>
+        /// <returns></returns>
+        public Node GetPositivePole()
+        {
+            return RightNode;
+        }
+
+        /// <summary>
+        /// 获取负极，默认为左节点
+        /// </summary>
+        /// <returns></returns>
+        public Node GetNegativePole()
+        {
+            return LeftNode;
+        }
+    }
+}
---
+++ Assets/Class/Frame/EletronicUnit.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using LinearAlgebra;
+using LinearAlgebra.LinearEquations;
+using Assets.Class.Entity;
+
+namespace Assets.Class.Frame
+{
+    public abstract class EletronicUnit
+    {
+        public double I { get; protected set; }//电流
+        public double R { get; protected set; }//电阻
+        public double V { get; protected set; }//电压
+
+        public Node LeftNode { get; protected set; }
+        public Node RightNode { get; protected set; }
+        
+
+        public EletronicUnit()
+        {
+            LeftNode = new Node(this); ;
+            RightNode = new Node(this); ;
+        }
+
+        public virtual Node GetOutNode(Node inNode)
+        {
+            if(LeftNode == inNode)
+            {
+                return RightNode;
+            }
+            return LeftNode;
+        }
+
+        internal void SetI(double i)
+        {
+            I = i;
+            V = i * R;
+        }
+        
+
+        
+    }
+
+    
+}
+
