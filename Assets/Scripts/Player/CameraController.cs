ee1a2462a7888db28a4c291842f23ac0c7d9e4d1
Yunlin <mao.looper@qq.com>
2020-02-11 21:26:46 +0800

    add pick up things

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -15,7 +15,7 @@ public class CameraController : MonoBehaviour
 
     [SerializeField] GameObject player;
     [SerializeField] Vector3 offset; //距离玩家的位移
-    [SerializeField] float maxDetectDistance = 100f;
+    [SerializeField] float maxDetectDistance = 2f;
 
     //镜头旋转
     [SerializeField] private float m_sensitivityX = 3f;
@@ -31,6 +31,13 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float zoomSpeed = 100f;
     [SerializeField] private float m_minDistance = 0f;
     [SerializeField] private float m_maxDistance = 100f;
+
+    // 要改变的鼠标样式图片
+    public Texture2D cursorTextureHand;
+    private CursorMode cursorMode = CursorMode.Auto;
+    private Vector2 hotSpot = Vector2.zero;
+
+    private GameObject pickGameObj = null;
 
     float m_rotationY = 0f;
 
@@ -54,29 +61,70 @@ public class CameraController : MonoBehaviour
         CameraRotate();
         //CameraFOV();
         FollowPlayer();
-        DetectEquipent();
+        DetectEquipent("Plane", "Equipment");
     }
 
-    private void DetectEquipent()
+    /// <summary>
+    /// 检测碰撞,并拖拽
+    /// </summary>
+    private void DetectEquipent(string planeTag, string targetTag)
     {
         //屏幕中间发射射线
-        //Vector3 screenMid = new Vector3(Screen.width / 2, Screen.height / 2, 0f);
-        //Ray ray = Camera.main.ScreenPointToRay(screenMid);
+        //Vector3 targetPos = new Vector3(Screen.width / 2, Screen.height / 2, 0f);
         //鼠标位置发射射线
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
-        
-        RaycastHit raycastHit;
+        Vector3 targetPos = Input.mousePosition;
+        Ray ray = Camera.main.ScreenPointToRay(targetPos);
+        RaycastHit hitInfo;
 
-        if (Physics.Raycast(ray, out raycastHit))
+        Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+        //目前没有拾起物品
+        if (pickGameObj == null)
         {
-            if (raycastHit.collider.gameObject.tag=="Equipment"&&raycastHit.distance < maxDetectDistance)
+            if (Physics.Raycast(ray, out hitInfo))
             {
-                Debug.Log("碰撞成功");
+                if (hitInfo.collider.gameObject.CompareTag(targetTag))
+                {
+                    Debug.Log("碰撞成功" + hitInfo.collider.name);
+                    //切换鼠标样式
+                    //Cursor.SetCursor(cursorTextureHand, hotSpot, cursorMode);
+
+                    if (Input.GetMouseButton(0))
+                    {
+                        //存储被抓取的对象
+                        pickGameObj = hitInfo.collider.gameObject;
+                    }
+                }
             }
+        }
+        
+        //如果有拾取的物体，并且鼠标与地面碰撞，则使物体移动
+        if (pickGameObj != null)
+        {
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                if (hitInfo.collider.gameObject.CompareTag(planeTag))
+                {
+                    pickGameObj.GetComponent<Rigidbody>().freezeRotation = true;
+                    pickGameObj.transform.position = new Vector3(
+                        hitInfo.point.x,
+                        hitInfo.point.y,
+                        hitInfo.point.z + pickGameObj.GetComponent<Collider>().bounds.size.y / 2);
+                }
+                
+                Debug.Log("第二次发射" + hitInfo.collider.name);
+            }
+        }
+        //释放物体
+        if (Input.GetMouseButtonUp(0))
+        {
+            pickGameObj.GetComponent<Rigidbody>().freezeRotation = false;
+            pickGameObj = null;
         }
     }
 
+    /// <summary>
+    /// 跟随玩家
+    /// </summary>
     private void FollowPlayer()
     {
         transform.position = player.transform.position + offset;
