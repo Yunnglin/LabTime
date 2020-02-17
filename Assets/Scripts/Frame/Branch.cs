bf42d5beecaf90449d52a20fde6040c77914cdb1
Yunlin <mao.looper@qq.com>
2020-02-17 23:20:43 +0800

    re update

---
+++ Assets/Scripts/Frame/Branch.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Class.Frame
+{
+    class Branch
+    {
+        public Node PP = null;
+        public Node NP = null;
+
+        public double R = 0;
+        public double I = 0;
+        public double U = 0;
+
+        public List<EletronicUnit> Units { get; private set; }
+
+        public Branch(Node node)
+        {
+            PP = node;
+            Units = new List<EletronicUnit>();
+        }
+
+        public double Count
+        {
+            get
+            {
+                return Units.Count;
+            }
+        }
+
+        public void Append(EletronicUnit unit)
+        {
+            Units.Add(unit);
+            R = R + unit.R;
+        }
+
+        public void Insert(int index, EletronicUnit unit)
+        {
+            Units.Insert(index, unit);
+        }
+
+        public void SetI(double I)
+        {
+            this.I = I;
+            U = I * R;
+            foreach(EletronicUnit unit in Units)
+            {
+                unit.SetI(I);
+            }
+        }
+
+    }
+}
---
+++ Assets/UGUI/Scirpts/SelectInClass.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectInClass : MonoBehaviour
+{
+
+    public GameObject Main;
+    public GameObject Menu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void Selected()
+    {
+        Menu.SetActive(true);
+        Main.SetActive(false);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
